cae4cf3a4cc53c8da41ae9504f3a9b4f5fe5492e
Prajwol Gyawali <prajwol@alpenlabs.io>
2026-03-23 08:15:14 +0545

    feat: add block integrity logic

--- crates/btc-verification/src/lib.rs
+++ crates/btc-verification/src/lib.rs
@@ -2,11 +2,12 @@
 
 mod body_verification;
 pub mod header_verification;
-mod inclusion_proof;
+pub mod inclusion_proof;
 pub mod timestamp_store;
 pub mod utils_btc;
 pub mod work;
 
+pub use body_verification::check_block_integrity;
 pub use header_verification::*;
 pub use timestamp_store::*;
 pub use utils_btc::*;
