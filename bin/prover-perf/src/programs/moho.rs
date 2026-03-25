d6225190912b46130e58850e40bd5060f07af3e6
Prajwol Gyawali <prajwol@alpenlabs.io>
2026-03-25 13:49:07 +0545

    feat: moho recursive proof guest (#12)
    * feat: moho recursive proof guest
    
    * chore: update moho and zkaleido dep
    
    * feat: add moho proof report
    
    * ci: run moho guest program
    
    * chore: add a todo

---
+++ bin/prover-perf/src/programs/moho.rs
@@ -0,0 +1,25 @@
+use std::fs;
+
+use moho_recursive_proof::{
+    test_utils::{create_input, SchnorrPredicate},
+    MohoRecursiveProgram,
+};
+use zkaleido::{PerformanceReport, ZkVmProgramPerf};
+use zkaleido_sp1_host::SP1Host;
+
+const MOHO_ELF_PATH: &str = env!("SP1_ELF_guest-sp1-moho");
+
+fn load_elf() -> Vec<u8> {
+    fs::read(MOHO_ELF_PATH)
+        .unwrap_or_else(|err| panic!("failed to read guest elf at {MOHO_ELF_PATH}: {err}"))
+}
+
+pub(crate) fn gen_perf_report() -> PerformanceReport {
+    // TODO(STR-2797): Use Groth16Predicate instead of SchnorrPredicate
+    let moho = SchnorrPredicate::new();
+    let step = SchnorrPredicate::new();
+    let input = create_input(1, 2, None, &moho, &step);
+    let elf = load_elf();
+    let host = SP1Host::init(&elf);
+    MohoRecursiveProgram::perf_report(&input, &host).expect("failed to generate performance report")
+}
