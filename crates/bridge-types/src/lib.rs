1dbb5ac76410ed09ff6ed317a17b089936fbc85e
Prajwol Gyawali <prajwol@alpenlabs.io>
2026-04-03 07:47:28 +0545

    chore: remove strata-ol-stf as a dependency by adding bridge gateway account id and serial in strata-bridge-types

--- crates/bridge-types/src/lib.rs
+++ crates/bridge-types/src/lib.rs
@@ -1,2 +1,12 @@
+use strata_identifiers::{AccountId, AccountSerial};
+
 mod operator;
 pub use operator::{OperatorIdx, OperatorSelection};
+
+const BRIDGE_GATEWAY_REF: u8 = 0x10;
+
+/// Account ID that we use for the bridge gateway account.
+pub const BRIDGE_GATEWAY_ACCT_ID: AccountId = AccountId::special(BRIDGE_GATEWAY_REF);
+
+/// Serial of the bridge gateway account.
+pub const BRIDGE_GATEWAY_ACCT_SERIAL: AccountSerial = AccountSerial::reserved(BRIDGE_GATEWAY_REF);
