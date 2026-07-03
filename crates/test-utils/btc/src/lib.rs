468cee60853ec4244195e6e927c59d2c35d68f8d
Jose Storopoli <jose@storopoli.com>
2026-07-03 08:39:18 -0300

    chore(deps): bump rust to nightly-2026-07-01 (#176)
    * chore(deps): bump nightly to 2027-07-01
    
    * chore: automated clippy lints
    
    * chore: fmt

--- crates/test-utils/btc/src/lib.rs
+++ crates/test-utils/btc/src/lib.rs
@@ -80,7 +80,12 @@ impl BtcMainnetSegment {
             (MAINNET_FIXTURE_END_HEIGHT - MAINNET_FIXTURE_START_HEIGHT) as usize + 1,
         );
 
-        for (idx, chunk) in raw_headers.chunks_exact(Header::SIZE).enumerate() {
+        for (idx, chunk) in raw_headers
+            .as_chunks::<{ Header::SIZE }>()
+            .0
+            .iter()
+            .enumerate()
+        {
             let height = MAINNET_FIXTURE_START_HEIGHT + idx as L1Height;
             let header =
                 deserialize(chunk).expect("valid serialized header in bundled fixture range");
